34de7526f34a840d5b18d89c678a1c563ed97f67
Nicholas Mancuso <nick.mancuso@gmail.com>
2016-04-26 14:07:16 -0700

    added gwas script

---
+++ hw1/src/gwas.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import argparse as ap
+import os
+import sys
+
+import numpy as np
+import scipy.stats as stats
+
+
+def main(args):
+    argp = ap.ArgumentParser(description="")
+    argp.add_argument("geno", type=ap.FileType("r"))
+    argp.add_argument("pheno", type=ap.FileType("r"))
+    argp.add_argument("-o", "--output", type=ap.FileType("w"),
+                      default=sys.stdout)
+
+    args = argp.parse_args(args)
+
+    ys = np.loadtxt(args.pheno)
+
+    bcf = 382 * 4
+    pvals = [[] for _ in range(4)]
+    lines = args.geno.readlines()
+
+    for idx, y in enumerate(ys.T):
+        for sdx, line in enumerate(lines):
+            snp = np.array(list(line.strip()), dtype=float)
+            beta, bias, rho, pval, stderr = stats.linregress(snp, y)
+            pvals[idx].append(pval)
+            if pval < 0.05 / bcf:
+                print idx + 1, sdx + 1, pval
+
+    pvals = np.array(pvals)
+    np.savetxt(args.output, pvals.T, fmt="%s")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
